cfc2b1c45a231640a64743705142b3c16d5523e6
Ansh-dhama <dhamaansh19@gmail.com>
2026-09-12 14:24:13 +0530

    Replace Gmail SMTP with Resend email API

--- demo/src/main/java/LogiTrack/Services/EmailService.java
+++ demo/src/main/java/LogiTrack/Services/EmailService.java
@@ -1,29 +1,61 @@
 package LogiTrack.Services;
 
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
 @Service
 @Slf4j
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender javaMailSender; // IntelliJ will turn green after Maven reload
+    @Value("${resend.api.key}")
+    private String resendApiKey;
+
     @Async
     public void sendEmail(String to, String subject, String body) {
+
         try {
-            SimpleMailMessage mail = new SimpleMailMessage();
-            mail.setTo(to);
-            mail.setSubject(subject);
-            mail.setText(body);
-            System.out.println("Sending email to: " + to);
-            javaMailSender.send(mail);
-            log.info("Email sent successfully to: {}", to);
-        } catch (Exception e) {
-            log.error("Failed to send email to {}: {}", to, e.getMessage());
+
+            Resend resend = new Resend(resendApiKey);
+
+            CreateEmailOptions email =
+                    CreateEmailOptions.builder()
+                            .from("LogiTrack <onboarding@resend.dev>")
+                            .to(to)
+                            .subject(subject)
+                            .html(
+                                    "<div style='font-family:Arial,sans-serif;'>"
+                                            + body.replace("\n", "<br>")
+                                            + "</div>"
+                            )
+                            .build();
+
+            log.info("Sending email to: {}", to);
+
+            CreateEmailResponse response =
+                    resend.emails().send(email);
+
+            log.info(
+                    "Email sent successfully to: {} | Resend ID: {}",
+                    to,
+                    response.getId()
+            );
+
+        } catch (ResendException e) {
+
+            log.error(
+                    "Failed to send email to {}: {}",
+                    to,
+                    e.getMessage(),
+                    e
+            );
         }
     }
 }
